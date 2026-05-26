9948e0144c0cc865c3026b6a30e1bcd5a3152251
gearsroboticsstaug <gearsroboticsstaug@gmail.com>
2026-05-26 10:03:36 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,25 @@
+let Temperature = 0
+led.enable(false)
+OLED.init(128, 64)
+OLED.writeStringNewLine("GEARS IS #1")
+let Strip = neopixel.create(DigitalPin.P3, 1, NeoPixelMode.RGB)
+basic.forever(function () {
+    Temperature = smarthome.ReadTemperature(TMP36Type.TMP36_temperature_F, AnalogPin.P1)
+    OLED.clear()
+    OLED.writeString("Temperature:")
+    OLED.writeNum(Temperature)
+    if (Temperature >= 65) {
+        OLED.newLine()
+        OLED.writeStringNewLine("T")
+    } else {
+        OLED.newLine()
+        OLED.writeStringNewLine("F")
+    }
+    basic.pause(100)
+})
+basic.forever(function () {
+	
+})
+basic.forever(function () {
+	
+})
