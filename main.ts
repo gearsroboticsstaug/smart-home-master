50b99c7fb2dcf6d2168625a5c000c0c2268d9fa5
gearsroboticsstaug <gearsroboticsstaug@gmail.com>
2026-05-26 11:08:23 -0400

    Update main.ts using claude

--- main.ts
+++ main.ts
@@ -1,12 +1,11 @@
 OLED.init(128, 64)
-let Light = smarthome.ReadLightIntensity(AnalogPin.P1)
-let Temperature = smarthome.ReadTemperature(TMP36Type.TMP36_temperature_F, AnalogPin.P8)
+let Light = 0
+let Temperature = 0
 let Strip = neopixel.create(DigitalPin.P2, 1, NeoPixelMode.RGB)
-basic.forever(function () {
-	
-})
+
 basic.forever(function () {
     Temperature = smarthome.ReadTemperature(TMP36Type.TMP36_temperature_F, AnalogPin.P8)
+    OLED.clear()
     OLED.writeString("Temp:")
     OLED.writeNum(Temperature)
     if (Temperature > 70) {
@@ -14,4 +13,5 @@ basic.forever(function () {
     } else {
         smarthome.motorFan(AnalogPin.P16, false)
     }
+    basic.pause(100)
 })
